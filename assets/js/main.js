cbb29fc7bd2a5c9c794cb8a1adeb740db05b002e
benitezmarcos <marcosbenitez@Marcoss-MacBook-Pro.local>
2019-10-21 16:18:00 -0400

    gallery added

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+  // Magnific Popup
+  $('.parent-container').magnificPopup({
+    delegate: 'a', // child items selector, by clicking on it popup will open
+    type: 'image',
+
+    gallery:{
+      enabled:true
+    }
+    // other options
+  });
+});
+
