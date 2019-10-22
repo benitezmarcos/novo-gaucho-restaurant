9389f76f034390e86d905d18867f8811ba8ccd24
benitezmarcos <marcosbenitez@Marcoss-MacBook-Pro.local>
2019-10-22 18:57:21 -0400

    finished all sections

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,38 @@
 $(document).ready(function () {
 
+  //toggler
+  $('.navbar-toggler').click(function(){
+    $('.navbar-toggler').toggleClass('change')
+  })
+
+  //sticky navbar
+  $(window).scroll(function(){
+    let position = $(this).scrollTop();
+    
+    if(position>= 551){
+      $('.navbar').addClass('navbar-background');
+      $('.navbar').addClass('fixed-top');
+    }
+    else{
+      $('.navbar').removeClass('navbar-background');
+      $('.navbar').removeClass('fixed-top');
+    }
+  })
+
+  //smooth scroll
+  // $('.nav-item a').click(function(link){
+  //   link.preventDefault();
+
+  //   let target = $(this).attr('href');
+
+  //   $('html, body').animate({
+  //     scrollTop: $(target).offset().top -25
+  //   }, 3000);
+  // })
+
+
+
+
   // Magnific Popup
   $('.parent-container').magnificPopup({
     delegate: 'a', // child items selector, by clicking on it popup will open
